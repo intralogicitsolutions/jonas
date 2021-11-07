99947889e59da400b14194632a08fef21bb54712
intralogicitsolutions <intralogicitsolutions@gmail.com>
2021-11-07 16:08:35 +0530

    -- modal setup

--- src/app/dashboard/manage-frameworks/manage-frameworks.component.ts
+++ src/app/dashboard/manage-frameworks/manage-frameworks.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+declare var $: any;
+export interface Fruit {
+  name: string;
+}
 @Component({
   selector: 'app-manage-frameworks',
   templateUrl: './manage-frameworks.component.html',
   styleUrls: ['./manage-frameworks.component.scss']
 })
 export class ManageFrameworksComponent implements OnInit {
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  fruits: Fruit[] = [];
 
-  constructor() { }
-
+  constructor(private _snackBar: MatSnackBar) { }
+  selectedOptions: any = [];
   ngOnInit(): void {
   }
+  openSnackBar() {
+    const snackBarRef = this._snackBar.open(this.selectedOptions.length + ' actions selected.', 'ASSIGN RESPONSIBLE');
+    snackBarRef.onAction().subscribe(() => {
+      console.log('The snackbar action was triggered!');
+      $('#exampleModal1').modal('show');
+    });
+  }
+  onChange(event: any): void {
+    this.openSnackBar()
+  }
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    // Add our fruit
+    if (value) {
+      this.fruits.push({ name: value });
+    }
+    // Clear the input value
+    event.chipInput!.clear();
+  }
 
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -9,9 +9,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
   declarations: [],
   imports: [
@@ -26,9 +29,16 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatProgressBarModule,
     MatGridListModule,
     CdkAccordionModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatInputModule,
+    MatChipsModule
   ],
-  exports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatButtonModule,
-    MatFormFieldModule, MatCardModule, MatProgressBarModule, MatGridListModule,CdkAccordionModule,MatCheckboxModule]
+  exports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatButtonModule, MatSnackBarModule,
+    FormsModule,
+    MatInputModule,
+    MatChipsModule,
+    MatFormFieldModule, MatCardModule, MatProgressBarModule, MatGridListModule, CdkAccordionModule, MatCheckboxModule]
 })
 export class SharedModule { }
